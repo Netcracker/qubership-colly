742471475857ce9e6b2bae71cb71f59ae23d612c
Roman Romanov <ormigus@gmail.com>
2025-07-29 18:35:49 +0300

    feat: refactor environment table toolbar into a separate component

--- src/main/webui/src/components/EnvTable.tsx
+++ src/main/webui/src/components/EnvTable.tsx
@@ -1,21 +1,8 @@
 import React, {useCallback, useEffect, useMemo, useState} from "react";
-import {Badge, Box, Checkbox, Chip, FormControlLabel, InputAdornment, TextField, Tooltip} from "@mui/material";
-import ViewColumnIcon from '@mui/icons-material/ViewColumn';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import CancelIcon from '@mui/icons-material/Cancel';
-import SearchIcon from '@mui/icons-material/Search';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import {Box, Chip} from "@mui/material";
 import {
-    ColumnsPanelTrigger,
     DataGrid,
-    FilterPanelTrigger,
     GridColDef,
-    QuickFilter,
-    QuickFilterClear,
-    QuickFilterControl,
-    Toolbar,
-    ToolbarButton,
     useGridApiRef
 } from '@mui/x-data-grid';
 import {Environment, ENVIRONMENT_TYPES_MAPPING, EnvironmentStatus, STATUS_MAPPING} from "../entities/environments";
@@ -23,6 +10,7 @@ import {UserInfo} from "../entities/users";
 import dayjs from "dayjs";
 import ConfirmationDialog from "./ConfirmDialog";
 import EditEnvironmentDialog from "./EditEnvironmentDialog";
+import EnvTableToolbar from "./EnvTableToolbar";
 import {Namespace} from "../entities/namespaces";
 
 interface EnvTableProps {
@@ -252,98 +240,15 @@ export default function EnvTable({userInfo, monitoringColumns}: EnvTableProps) {
 
     const CustomToolbar = () => {
         return (
-            <Toolbar>
-                <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-                    <QuickFilter>
-                        <QuickFilterControl
-                            render={({ref, ...controlProps}, state) => (
-                                <TextField
-                                    {...controlProps}
-                                    inputRef={ref}
-                                    aria-label="Search"
-                                    placeholder="Search..."
-                                    size="small"
-                                    sx={{ width: 250, minWidth: 250 }}
-                                    slotProps={{
-                                        input: {
-                                            startAdornment: (
-                                                <InputAdornment position="start">
-                                                    <SearchIcon fontSize="small"/>
-                                                </InputAdornment>
-                                            ),
-                                            endAdornment: state.value ? (
-                                                <InputAdornment position="end">
-                                                    <QuickFilterClear
-                                                        edge="end"
-                                                        size="small"
-                                                        aria-label="Clear search"
-                                                    >
-                                                        <CancelIcon fontSize="small"/>
-                                                    </QuickFilterClear>
-                                                </InputAdornment>
-                                            ) : null,
-                                            ...controlProps.slotProps?.input,
-                                        },
-                                        ...controlProps.slotProps,
-                                    }}
-                                />
-                            )}
-                        />
-                    </QuickFilter>
-
-                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                        {userInfo.authenticated && userInfo.isAdmin && (
-                            <Tooltip title="Edit">
-                                <ToolbarButton
-                                    size="medium"
-                                    onClick={() => setShowEditDialog(true)}
-                                    disabled={!selectedEnvironment}>
-                                    <EditIcon fontSize="small"/>
-                                </ToolbarButton>
-                            </Tooltip>
-                        )}
-                        {userInfo.authenticated && userInfo.isAdmin && (
-                            <Tooltip title="Delete">
-                                <ToolbarButton
-                                    size="medium"
-                                    onClick={() => setShowConfirmPopup(true)}
-                                    disabled={!selectedEnvironment}>
-                                    <DeleteIcon fontSize="small"/>
-                                </ToolbarButton>
-                            </Tooltip>
-                        )}
-
-                        <Tooltip title="Columns">
-                            <ColumnsPanelTrigger render={<ToolbarButton/>}>
-                                <ViewColumnIcon fontSize="small"/>
-                            </ColumnsPanelTrigger>
-                        </Tooltip>
-
-                        <Tooltip title="Filters">
-                            <FilterPanelTrigger
-                                render={(props, state) => (
-                                    <ToolbarButton {...props} color="default">
-                                        <Badge badgeContent={state.filterCount} color="primary" variant="dot">
-                                            <FilterListIcon fontSize="small"/>
-                                        </Badge>
-                                    </ToolbarButton>
-                                )}
-                            />
-                        </Tooltip>
-                        <Tooltip title="Show/Hide Unexisting Namespaces">
-                            <FormControlLabel
-                                control={<Checkbox
-                                    checked={showAllNamespaces}
-                                    onChange={(event) => {
-                                        setShowAllNamespaces(event.target.checked);
-                                    }}/>}
-                                label={"Show All Namespaces"}
-                            />
-                        </Tooltip>
-                    </Box>
-                </Box>
-            </Toolbar>
-        )
+            <EnvTableToolbar
+                userInfo={userInfo}
+                selectedEnvironment={selectedEnvironment}
+                showAllNamespaces={showAllNamespaces}
+                onShowEditDialog={() => setShowEditDialog(true)}
+                onShowConfirmPopup={() => setShowConfirmPopup(true)}
+                onShowAllNamespacesChange={setShowAllNamespaces}
+            />
+        );
     };
 
     if (loading) {
