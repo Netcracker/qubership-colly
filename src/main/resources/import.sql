2b25fd57d495757ba6158ee75f9609c7d3c9bf48
Roman Romanov <ormigus@gmail.com>
2025-08-26 10:51:51 +0500

    fix: add sql-load-script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
 -- Migration script to change deploymentVersion column type from VARCHAR(255) to TEXT
 ALTER TABLE environments ALTER COLUMN deploymentVersion TYPE TEXT;
+ALTER TABLE environments ALTER COLUMN description TYPE TEXT;
