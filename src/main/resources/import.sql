67cd1fc1160d4e1ccfed16ec68f7a2fa8320a0d2
Roman Romanov <ormigus@gmail.com>
2025-08-26 11:27:42 +0500

    Revert "fix: add sql-load-script"
    This reverts commit 2b25fd57d495757ba6158ee75f9609c7d3c9bf48.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,2 @@
 -- Migration script to change deploymentVersion column type from VARCHAR(255) to TEXT
 ALTER TABLE environments ALTER COLUMN deploymentVersion TYPE TEXT;
-ALTER TABLE environments ALTER COLUMN description TYPE TEXT;
