5b341653e64b8cf531bffd42f8b19782f6fe654b
Roman Romanov <ormigus@gmail.com>
2026-01-22 15:10:28 +0300

    feat: add number of nodes to Cluster and update related classes

--- environment-operational-service/src/main/java/org/qubership/colly/db/data/Cluster.java
+++ environment-operational-service/src/main/java/org/qubership/colly/db/data/Cluster.java
@@ -1,63 +1,19 @@
 package org.qubership.colly.db.data;
 
-import java.util.ArrayList;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@Builder
 public class Cluster {
     private String id;
     private String name;
     private Boolean synced;
+    private Integer numberOfNodes;
     private List<String> environmentIds;
     private List<String> namespaceIds;
-
-    public Cluster(String id, String name) {
-        this.id = id;
-        this.name = name;
-        this.synced = false;
-        this.namespaceIds = new ArrayList<>();
-        this.environmentIds = new ArrayList<>();
-    }
-
-    public Cluster() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean getSynced() {
-        return synced != null && synced;
-    }
-
-    public void setSynced(Boolean synced) {
-        this.synced = synced;
-    }
-
-    public List<String> getEnvironmentIds() {
-        return environmentIds;
-    }
-
-    public void setEnvironmentIds(List<String> environmentIds) {
-        this.environmentIds = environmentIds;
-    }
-
-    public List<String> getNamespaceIds() {
-        return namespaceIds;
-    }
-
-    public void setNamespaceIds(List<String> namespaceIds) {
-        this.namespaceIds = namespaceIds;
-    }
 }
