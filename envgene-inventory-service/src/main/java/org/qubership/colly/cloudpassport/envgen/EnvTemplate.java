a799177f58734918212d1b8e1637aa95c7f2f7e3
Roman Romanov <ormigus@gmail.com>
2026-03-27 17:25:25 +0300

    fix: add env-template-test definition and ignore unknown properties in EnvTemplate

--- envgene-inventory-service/src/main/java/org/qubership/colly/cloudpassport/envgen/EnvTemplate.java
+++ envgene-inventory-service/src/main/java/org/qubership/colly/cloudpassport/envgen/EnvTemplate.java
@@ -1,8 +1,11 @@
 package org.qubership.colly.cloudpassport.envgen;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 import java.util.Map;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record EnvTemplate(Map<String, List<String>> envSpecificParamsets,
                           Map<String, List<String>> envSpecificTechnicalParamsets,
                           Map<String, List<String>> envSpecificE2EParamsets) {
